d89b27440ff0b985dddb92aa3dbc8904cae5094a
OIRNOIR <oirnoir@outlook.com>
2022-01-20 15:32:38 -0800

    bonk

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 window.onload = () => {
   const urlParams = new URLSearchParams(location.search);
   if (urlParams.has("thisIsAVeryLongLink")) {
-    window.history.replaceState(null, "", "/");
     const step1 = atob(urlParams.get('thisIsAVeryLongLink'));
     let step2 = [];
     for (let i = 0; i < step1.length; i += 4) {
