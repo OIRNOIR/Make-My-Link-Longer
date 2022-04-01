af98c3ca9277ae667670cff0a915b30536d8917a
OIRNOIR <oirnoir@outlook.com>
2022-04-01 09:24:22 -0700

    boop

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 window.onload = () => {
   const urlParams = new URLSearchParams(location.search);
   if (urlParams.has("thisIsAVeryLongLink")) {
-    const step1 = atob(urlParams.get('thisIsAVeryLongLink'));
-    let step2 = [];
-    for (let i = 0; i < step1.length; i += 4) {
-      step2.push(step1.split("")[i]);
-    }
-    let h = atob(step2.join(""));
-    if (!h.startsWith("http")){
-      h = "https://" + h;
-    }
-    window.location.href = h;
-  }
+		try {
+			const step1 = atob(urlParams.get('thisIsAVeryLongLink'));
+			let step2 = [];
+			for (let i = 0; i < step1.length; i += 4) {
+				step2.push(step1.split("")[i]);
+			}
+			let h = atob(step2.join(""));
+			if (!h.startsWith("http")){
+				h = "https://" + h;
+			}
+			window.location.href = h;
+		} catch {
+			document.getElementById("title").innerText = "This link is invalid!";	
+		}
+  } else {
+		document.getElementById("title").innerText = "This link is invalid!";
+	}
 }
