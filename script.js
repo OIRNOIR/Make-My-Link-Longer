3ed3479249b7d40d3c65c66de1e3868af59fd5c3
OIRNOIR <oirnoir@outlook.com>
2022-08-09 14:25:03 -0500

    ree of course

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function generate(){
 		}
 	}
 	const finalUrl = btoa(step2.join(""));
-	currentURL = "https://very-very-very-very-very-very-very-very-very-long-link.make-my-link-longer.ml/this/link/is/really/quite/long/its-actually-quite-impressive/the-sheer-length-of-this-link/?longlink=true&areyoureallysurprised=no&whyareyourunning=idk&thisIsAVeryLongLink=" + finalUrl;
+	currentURL = "https://very-very-very-very-very-very-very-very-very-long-link.make-my-link-longer.ml/this/link/is/really/quite/long/its-actually-quite-impressive/the-sheer-length-of-this-link/?longlink=true&areyoureallysurprised=no&whyareyourunning=idk&thisIsAVeryLongLink=" + finalUrl + "&explanation=none&context=0";
 	document.getElementById("generated").style.display = "block";
 	document.getElementById("output").innerText = currentURL;
 }
