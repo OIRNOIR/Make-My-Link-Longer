cf2c8484fc408f7ce4370a9d31e5d837423f025b
OIRNOIR <oirnoir@outlook.com>
2022-08-11 12:29:52 -0500

    Fix lol

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function generate(){
 		}
 	}
 	const finalUrl = btoa(step2.join(""));
-	currentURL = "https://very-very-very-very-very-very-very-very-very-long-link.make-my-link-longer.ml/this/link/is/really/quite/long/its-actually-quite-impressive/the-sheer-length-of-this-link/?longLink=true&areYouReallySurprised=iThoughtNot&whyAreYouRunning=idk&thisIsAVeryLongLink=" + finalUrl + "&explanation=none&context=0&utm_source=facebook&utm_medium=main&utm_campaign=THIS_TOTALLY_UNOBTRUSIVE_CAMPAIGN_NAME&utm_content=some_sad_place_on_the_internet&more_ridiculous_things_in_your_links=because_why_not&maybe_we_can=write_poetry_in_utm_links&utm_utm_utm_utm_utm_param=bye&oh_and_dont_forget_the_facebook_click_id=thanks";
+	currentURL = "https://very-very-very-very-very-very-very-very-very-long-link.make-my-link-longer.ml/this/link/is/really/quite/long/its-actually-quite-impressive/the-sheer-length-of-this-link/?longLink=true&areYouReallySurprised=iThoughtNot&whyAreYouRunning=idk&thisIsAVeryLongLink=" + encodeURIComponent(finalUrl) + "&explanation=none&context=0&utm_source=facebook&utm_medium=main&utm_campaign=THIS_TOTALLY_UNOBTRUSIVE_CAMPAIGN_NAME&utm_content=some_sad_place_on_the_internet&more_ridiculous_things_in_your_links=because_why_not&maybe_we_can=write_poetry_in_utm_links&utm_utm_utm_utm_utm_param=bye&oh_and_dont_forget_the_facebook_click_id=thanks";
 	document.getElementById("generated").style.display = "block";
 	document.getElementById("output").innerText = currentURL;
 }
